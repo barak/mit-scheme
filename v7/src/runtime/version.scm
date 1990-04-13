cf79274c255d063c9681a74619c65d56d23f579b
Mark Friedman <edu/mit/csail/zurich/markf>
1990-04-13 14:14:58 +0000

    Added LOAD-LATEST and FASLOAD-LATEST to load the most recently modified of the LOAD/DEFAULT-TYPES (or FASLOAD/DEFAULT-TYPES). Also added a LOAD/DEFAULT-FIND-PATHNAME-WITH-TYPE variable which LOAD and FASLOAD will use (indirectly) to determine what file type to load. See the rcs log for load.scm for more info.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.78 1990/04/10 20:05:26 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.79 1990/04/13 14:14:58 markf Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 78))
+  (add-identification! "Runtime" 14 79))
 
 (define microcode-system)
 
