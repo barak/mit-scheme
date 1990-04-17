ceab07708d30a1a87fabef88e4bcf0ee94c332ba
Chris Hanson <org/chris-hanson/cph>
1990-04-17 22:20:34 +0000

    *** empty log message ***

--- v8/src/microcode/version.h
+++ v8/src/microcode/version.h
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v8/src/microcode/version.h,v 11.29 1990/04/12 22:47:59 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v8/src/microcode/version.h,v 11.30 1990/04/17 22:20:34 cph Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -46,7 +46,7 @@ MIT in each case. */
 #define VERSION		11
 #endif
 #ifndef SUBVERSION
-#define SUBVERSION	29
+#define SUBVERSION	30
 #endif
 
 #ifndef UCODE_TABLES_FILENAME
