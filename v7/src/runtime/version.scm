e7f6373b785939fd46119f11fd6f6bf355eaa79f
Chris Hanson <org/chris-hanson/cph>
1990-06-20 20:37:44 +0000

    * Changes to operating system interface to match those introduced in microcode version 11.33, which is required for this runtime system version.
    * The low-level channel abstraction has been changed -- it now
    contains only a microcode channel descriptor and the channel type.
    The microcode no longer knows the format of channels.
    
    * Subprocess support has been temporarily removed.  New subprocess
    support is being designed to accompany changes in the microcode.
    
    * The file-copy primitive is now written in Scheme using lower-level
    file system and I/O operations.
    
    * Use `input-port/immediate-mode' and `input-port/normal-mode' to
    switch terminal mode.  The operations `read-char-immediate' and
    `peek-char-immediate' are now obsolete.  REP loops force the input
    port into normal mode, while the debugger forces it into immediate
    mode.
    
    * I/O buffering for input files, output files, and the console output
    is now done in Scheme.  The microcode provides no buffering.
    These ports understand `buffer-size' and `set-buffer-size' operations
    to control the amount of buffering.  A buffer size of 0 disables
    buffering entirely.
    
    * `read-start!' and `read-finish!' are now optional operations on
    input ports.
    
    * An input or output port will now report the set of operations that
    it responds to.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.81 1990/06/14 00:01:49 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.82 1990/06/20 20:37:44 cph Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 81))
+  (add-identification! "Runtime" 14 82))
 
 (define microcode-system)
 
