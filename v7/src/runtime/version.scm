1a454a80defe6d146eab409bd4396b88dec7cdb0
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1990-06-28 16:35:40 +0000

    Better reporting of hardware traps. compiled-entry/block is the identity on compiled code blocks. Fixnum operations can now be constant folded by the syntaxer.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.85 1990/06/25 18:51:11 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.86 1990/06/28 16:35:40 jinx Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 85))
+  (add-identification! "Runtime" 14 86))
 
 (define microcode-system)
 
