27f28cbebe4718949096d2cad030aaf9e6487128
Chris Hanson <org/chris-hanson/cph>
1990-07-18 20:40:33 +0000

    Handle EOF on the console input.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.88 1990/07/16 17:12:56 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.89 1990/07/18 20:40:33 cph Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 88))
+  (add-identification! "Runtime" 14 89))
 
 (define microcode-system)
 
