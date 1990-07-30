327bb29e61cf44d64c6d11c3e8c27acc5a3a97bb
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1990-07-30 17:05:42 +0000

    Merge in Jmiller's changes for the MIPS.

--- v7/src/microcode/version.h
+++ v7/src/microcode/version.h
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/Attic/version.h,v 11.40 1990/07/28 18:57:10 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/Attic/version.h,v 11.41 1990/07/30 17:05:42 jinx Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -46,7 +46,7 @@ MIT in each case. */
 #define VERSION		11
 #endif
 #ifndef SUBVERSION
-#define SUBVERSION	40
+#define SUBVERSION	41
 #endif
 
 #ifndef UCODE_TABLES_FILENAME
