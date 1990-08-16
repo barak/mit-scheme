6230e7e0b78c02ced509f1a0f499ec90c5faa7b6
Chris Hanson <org/chris-hanson/cph>
1990-08-16 20:13:17 +0000

    Don't use `event:before-exit' to tell the microcode to clean itself up.  There is now a new hook at the microcode level that delays this cleanup until the `disk-restore' is committed.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.91 1990/08/08 00:58:42 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.92 1990/08/16 20:13:17 cph Exp $
 
 Copyright (c) 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 91))
+  (add-identification! "Runtime" 14 92))
 (define microcode-system)
 
 (define (snarf-microcode-version!)
