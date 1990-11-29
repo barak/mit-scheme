6eea63b32120037d5bd52ad6d7aa8726e2024d5c
Chris Hanson <org/chris-hanson/cph>
1990-11-29 00:18:23 +0000

    Pre-ANSI compilers don't handle automatic arrays declared as [].

--- v7/src/microcode/bchdmp.c
+++ v7/src/microcode/bchdmp.c
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/Attic/bchdmp.c,v 9.53 1990/11/29 00:12:09 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/Attic/bchdmp.c,v 9.54 1990/11/29 00:18:23 cph Rel $
 
 Copyright (c) 1987, 1988, 1989, 1990 Massachusetts Institute of Technology
 
@@ -743,8 +743,14 @@ DEFINE_PRIMITIVE ("PRIMITIVE-FASDUMP", Prim_prim_fasdump, 3, 3, 0)
     int copy_result;
     SCHEME_OBJECT fasdump_result;
     Tchannel channel, temp_channel;
-    char temp_name[19] = "/tmp/fasdumpXXXXXX";
-
+    char temp_name [19];
+    {
+      char * scan1 = "/tmp/fasdumpXXXXXX";
+      char * scan2 = temp_name;
+      while (1)
+	if (((*scan2++) = (*scan1++)) == '\0')
+	  break;
+    }
     channel = (arg_channel (2));
 
     (void) mktemp (temp_name);
