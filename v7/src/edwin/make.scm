252849e6f099022b2a0875358210b586b8a3bd27
Chris Hanson <org/chris-hanson/cph>
1991-05-15 19:11:51 +0000

    *** empty log message ***

--- v7/src/edwin/make.scm
+++ v7/src/edwin/make.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/edwin/make.scm,v 3.44 1991/05/14 02:29:09 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/edwin/make.scm,v 3.45 1991/05/15 19:11:51 cph Exp $
 
 Copyright (c) 1989-91 Massachusetts Institute of Technology
 
@@ -37,4 +37,4 @@ MIT in each case. |#
 (declare (usual-integrations))
 
 (package/system-loader "edwin" '() 'QUERY)
-(add-system! (make-system "Edwin" 3 44 '()))
+(add-system! (make-system "Edwin" 3 45 '()))
