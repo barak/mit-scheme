ebab7b0f6a1eb93f39a6606a89ab87cbbacd4188
Chris Hanson <org/chris-hanson/cph>
1991-05-15 19:32:13 +0000

    *** empty log message ***

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.121 1991/05/13 23:17:47 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.122 1991/05/15 19:32:13 cph Exp $
 
 Copyright (c) 1988-91 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 121))
+  (add-identification! "Runtime" 14 122))
 
 (define microcode-system)
 
