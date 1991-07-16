80bfee020383dec5dbafa5a3b889524dc636f6d9
Arthur Gleckler <edu/mit/csail/zurich/arthur>
1991-07-16 00:08:16 +0000

    Hooks for running before restarts and debugger returns.
    Debugger formatting improvements.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.124 1991/07/12 23:23:15 cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.125 1991/07/16 00:08:16 arthur Exp $
 
 Copyright (c) 1988-91 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 124))
+  (add-identification! "Runtime" 14 125))
 
 (define microcode-system)
 
