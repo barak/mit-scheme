e5eb5370da82617090aa14b141a209de9c9d1181
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1991-08-16 15:42:30 +0000

    New (runtime hash) module supports multiple hash tables.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.132 1991/08/16 01:21:21 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.133 1991/08/16 15:42:30 jinx Exp $
 
 Copyright (c) 1988-91 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 132))
+  (add-identification! "Runtime" 14 133))
 
 (define microcode-system)
 
