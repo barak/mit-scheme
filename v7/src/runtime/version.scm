ed08ce35485f9445c89ad500c39f34638a22944f
Arthur Gleckler <edu/mit/csail/zurich/arthur>
1991-08-26 15:25:13 +0000

    Make Scheme explain why it cannot open a file.
    Make EXIT and %EXIT take an optional argument for Scheme to use as
    UNIX status code.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.134 1991/08/22 15:19:05 arthur Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.135 1991/08/26 15:25:13 arthur Exp $
 
 Copyright (c) 1988-91 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 134))
+  (add-identification! "Runtime" 14 135))
 
 (define microcode-system)
 
