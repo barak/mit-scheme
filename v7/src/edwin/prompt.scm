c15141d205627a22957842fe591fe48d1690074d
Chris Hanson <org/chris-hanson/cph>
1991-10-21 12:06:00 +0000

    Make "m" key in Info mode default to the current menu item, if there is one.

--- v7/src/edwin/prompt.scm
+++ v7/src/edwin/prompt.scm
@@ -1,6 +1,6 @@
 ;;; -*-Scheme-*-
 ;;;
-;;;	$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/edwin/prompt.scm,v 1.149 1991/10/18 16:03:02 arthur Exp $
+;;;	$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/edwin/prompt.scm,v 1.150 1991/10/21 12:06:00 cph Exp $
 ;;;
 ;;;	Copyright (c) 1986, 1989-91 Massachusetts Institute of Technology
 ;;;
@@ -302,17 +302,12 @@
 (define (prompt-for-alist-value prompt alist #!optional default)
   (fluid-let ((map-name/external->internal identity-procedure)
 	      (map-name/internal->external identity-procedure))
-    (if (default-object? default)
-	(prompt-for-string-table-value prompt
-				       false
-				       'NO-DEFAULT
-				       (alist->string-table alist)
-				       true)
-	(prompt-for-string-table-value prompt
-				       default
-				       'VISIBLE-DEFAULT
-				       (alist->string-table alist)
-				       true))))
+    (prompt-for-string-table-value prompt
+				   (and (not (default-object? default))
+					default)
+				   'VISIBLE-DEFAULT
+				   (alist->string-table alist)
+				   true)))
 
 (define (prompt-for-command prompt)
   (fluid-let ((map-name/external->internal editor-name/external->internal)
