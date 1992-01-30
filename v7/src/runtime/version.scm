cf3cbb7e169aa914ee72b17d9f7f274f80fd0479
Arthur Gleckler <edu/mit/csail/zurich/arthur>
1992-01-30 17:08:31 +0000

    Change graphics operations to receive the device, not just the descriptor, of the graphics object on which they are operating.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.143 1991/12/19 20:51:42 arthur Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.144 1992/01/30 17:08:31 arthur Exp $
 
 Copyright (c) 1988-91 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 143))
+  (add-identification! "Runtime" 14 144))
 
 (define microcode-system)
 
