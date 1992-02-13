8b56e4a2d0926337947559547c299750949582f9
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1992-02-13 03:05:15 +0000

    More changes.

--- v7/src/compiler/machines/i386/rgspcm.scm
+++ v7/src/compiler/machines/i386/rgspcm.scm
@@ -1,7 +1,7 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/compiler/machines/i386/rgspcm.scm,v 1.1 1992/02/13 03:01:18 jinx Exp $
-$MC68020-Header: rgspcm.scm,v 4.1 87/12/30 07:05:38 GMT cph Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/compiler/machines/i386/rgspcm.scm,v 1.2 1992/02/13 03:01:52 jinx Exp $
+$MC68020-Header: /scheme/compiler/bobcat/RCS/rgspcm.scm,v 4.2 1991/05/06 23:17:03 jinx Exp $
 
 Copyright (c) 1992 Massachusetts Institute of Technology
 
