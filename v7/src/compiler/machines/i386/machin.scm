60c461773f318c8cf3e8f187a3ca3b1b7d567a83
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1992-02-18 22:57:25 +0000

    Remove FLONUM-ASIN and FLONUM-ACOS from open-coded list of primitives.

--- v7/src/compiler/machines/i386/machin.scm
+++ v7/src/compiler/machines/i386/machin.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/compiler/machines/i386/machin.scm,v 1.9 1992/02/17 22:37:34 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/compiler/machines/i386/machin.scm,v 1.10 1992/02/18 22:57:25 jinx Exp $
 $MC68020-Header: /scheme/src/compiler/machines/bobcat/RCS/machin.scm,v 4.26 1991/10/25 06:49:34 cph Exp $
 
 Copyright (c) 1992 Massachusetts Institute of Technology
@@ -344,4 +344,7 @@ MIT in each case. |#
   true)
 
 (define compiler:primitives-with-no-open-coding
-  '(DIVIDE-FIXNUM GCD-FIXNUM &/))
+  '(DIVIDE-FIXNUM GCD-FIXNUM &/
+		  ;; The rewriting rules in rulrew.scm don't work.
+		  ;; Treat as not available.
+		  FLONUM-ASIN FLONUM-ACOS))
