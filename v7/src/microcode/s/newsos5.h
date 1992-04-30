9adcfa38c78587d6622fa57a5cd54cb86d64bcbb
Chris Hanson <org/chris-hanson/cph>
1992-04-30 04:06:32 +0000

    Initial revision

---
+++ v7/src/microcode/s/newsos5.h
@@ -0,0 +1,40 @@
+/* -*-C-*-
+   System file for Sony NEWS-OS version 5
+
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/s/Attic/newsos5.h,v 1.1 1992/04/30 04:06:32 cph Exp $
+
+Copyright (c) 1992 Massachusetts Institute of Technology
+
+This material was developed by the Scheme project at the Massachusetts
+Institute of Technology, Department of Electrical Engineering and
+Computer Science.  Permission to copy this software, to redistribute
+it, and to use it for any purpose is granted, subject to the following
+restrictions and understandings.
+
+1. Any copy made of this software must include this copyright notice
+in full.
+
+2. Users of this software agree to make their best efforts (a) to
+return to the MIT Scheme project any improvements or extensions that
+they make, so that these may be included in future releases; and (b)
+to inform MIT of noteworthy uses of this software.
+
+3. All materials developed as a consequence of the use of this
+software shall duly acknowledge such use, in accordance with the usual
+standards of acknowledging credit in academic research.
+
+4. MIT has made no warrantee or representation that the operation of
+this software will be error-free, and MIT is under no obligation to
+provide any services, by way of maintenance, update, or otherwise.
+
+5. In conjunction with products arising from the use of this material,
+there shall be no use of the name of the Massachusetts Institute of
+Technology nor of any adaptation thereof in any advertising,
+promotional, or sales literature without prior written consent from
+MIT in each case. */
+
+#include "s/sysv4.h"
+
+/* This is required for version 5.0.2 or later.
+   Comment this out if you have 5.0.1.  */
+#define LD_SWITCH_SYSTEM -non_shared
