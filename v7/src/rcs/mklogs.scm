d08b5725fd5f12b30d6b777b5adccec0fd336bae
Chris Hanson <org/chris-hanson/cph>
1992-04-30 21:12:43 +0000

    Change to reflect source tree.

--- v7/src/rcs/mklogs.scm
+++ v7/src/rcs/mklogs.scm
@@ -9,12 +9,14 @@
 		     (map (lambda (subdirectory)
 			    (string-append subdirectory "/RCS"))
 			  subdirectories))))))
-  (make-log "/scheme/src/microcode" "m" "s")
-  (make-log "/scheme/src/runtime")
-  (make-log "/scheme/src/sf")
+  (make-log "/scheme/src/6001")
+  (make-log "/scheme/src/compiler" "back" "base" "fggen" "fgopt"
+	    "rtlbase" "rtlgen" "rtlopt" "machines/bobcat" "machines/i386"
+	    "machines/mips" "machines/spectrum" "machines/vax")
   (make-log "/scheme/src/cref")
   (make-log "/scheme/src/edwin")
-  (make-log "/scheme/src/sicp")
-  (make-log "/scheme/src/compiler" "back" "base" "fggen" "fgopt"
-	    "rtlbase" "rtlgen" "rtlopt" "machines/bobcat"
-	    "machines/mips" "machines/spectrum" "machines/vax"))
+  (make-log "/scheme/src/microcode" "m" "s")
+  (make-log "/scheme/src/rcs")
+  (make-log "/scheme/src/runtime")
+  (make-log "/scheme/src/sf")
+  (make-log "/scheme/src/sicp"))
