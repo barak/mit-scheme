d6384f93042ecdd84fd5d420a2b721282330a052
Henry M. Wu <edu/mit/csail/zurich/mhwu>
1992-05-26 20:33:14 +0000

    Add support for trimmed and compressed debugging information files.

--- v7/src/runtime/version.scm
+++ v7/src/runtime/version.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.152 1992/05/26 01:01:05 jinx Exp $
+$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/runtime/version.scm,v 14.153 1992/05/26 20:33:14 mhwu Exp $
 
 Copyright (c) 1988-1992 Massachusetts Institute of Technology
 
@@ -45,7 +45,7 @@ MIT in each case. |#
 		     '()))
   (add-system! microcode-system)
   (add-event-receiver! event:after-restore snarf-microcode-version!)
-  (add-identification! "Runtime" 14 152))
+  (add-identification! "Runtime" 14 153))
 
 (define microcode-system)
 
