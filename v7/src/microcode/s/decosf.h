c80781a0854a5552795e6ff398ebe988bfe4c904
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1992-08-29 12:47:54 +0000

    Define AUTOCLOBBER_BUG.

--- v7/src/microcode/s/decosf.h
+++ v7/src/microcode/s/decosf.h
@@ -1,10 +1,12 @@
 /* -*-C-*-
    System file for DEC OSF1
 
-$Id: decosf.h,v 1.1 1992/08/29 12:21:21 jinx Exp $
+$Id: decosf.h,v 1.2 1992/08/29 12:47:54 jinx Exp $
 
 Copyright (C) 1992  Digital Equipment Corporation */
 
 #define LIB_DEBUG		/* no -lg */
 
-#define C_SWITCH_SYSTEM -std1
+/* AUTOCLOBBER_BUG temporarily defined (until BL10) and release. */
+
+#define C_SWITCH_SYSTEM -std1 -DAUTOCLOBBER_BUG
