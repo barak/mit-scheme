4a89344813611fae76c0f98ef1d47b1240c78837
Chris Hanson <org/chris-hanson/cph>
1992-09-25 01:20:59 +0000

    Remove some now-unnecessary compiler switches.

--- v7/src/microcode/m/hp9k400.h
+++ v7/src/microcode/m/hp9k400.h
@@ -1,7 +1,7 @@
 /* -*-C-*-
    Machine file for HP9000 series 400 (or 300) with 68040
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/m/Attic/hp9k400.h,v 1.3 1992/03/16 19:55:09 cph Exp $
+$Id: hp9k400.h,v 1.4 1992/09/25 01:20:59 cph Exp $
 
 Copyright (c) 1991-92 Massachusetts Institute of Technology
 
@@ -67,13 +67,13 @@ MIT in each case. */
 /* The full optimizer breaks some of the files in those versions.  */
 /* #define C_OPTIMIZE_SWITCH +O1 */
 
-#define C_SWITCH_MACHINE -Wp,-H512000 -DTYPE_CODE_LENGTH=6 -DCOMPILER_PROCESSOR_TYPE=COMPILER_MC68040_TYPE
+#define C_SWITCH_MACHINE -Wp,-H512000 -DTYPE_CODE_LENGTH=6
 /* For hp-ux prior to 8.0, add the following switches to the previous line:
    -Wc,-Nt30000,-Ns3000  */
 
 #else
 
-#define C_SWITCH_MACHINE -DTYPE_CODE_LENGTH=6 -DCOMPILER_PROCESSOR_TYPE=COMPILER_MC68040_TYPE
+#define C_SWITCH_MACHINE -DTYPE_CODE_LENGTH=6
 
 #endif
 
