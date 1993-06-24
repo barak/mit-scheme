a9e9c959fa4028ad7aeb52c48b4234fac33f8388
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-06-24 02:00:59 +0000

    Fix date and RCS header.

--- v7/src/microcode/ntfs.c
+++ v7/src/microcode/ntfs.c
@@ -1,8 +1,8 @@
 /* -*-C-*-
 
-$Header: /Users/cph/tmp/foo/mit-scheme/mit-scheme/v7/src/microcode/ntfs.c,v 1.2 1993/06/24 01:52:11 gjr Exp $
+$Id: ntfs.c,v 1.3 1993/06/24 02:00:59 gjr Exp $
 
-Copyright (c) 1992 Massachusetts Institute of Technology
+Copyright (c) 1992-1993 Massachusetts Institute of Technology
 
 This material was developed by the Scheme project at the Massachusetts
 Institute of Technology, Department of Electrical Engineering and
