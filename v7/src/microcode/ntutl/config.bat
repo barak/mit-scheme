340592124e473fc13afe2a94c3fc9cc0afca28ff
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-07-18 20:30:44 +0000

    Copy setup.bat out of ntutl.

--- v7/src/microcode/ntutl/config.bat
+++ v7/src/microcode/ntutl/config.bat
@@ -3,10 +3,11 @@ rem MIT Scheme microcode configuration script for Windows NT
 rem
 rem Copyright (c) 1993 Massachusetts Institute of Technology
 rem
-rem $Id: config.bat,v 1.2 1993/06/24 15:18:20 gjr Exp $
+rem $Id: config.bat,v 1.3 1993/07/18 20:30:44 gjr Exp $
 rem
 copy cmpintmd\i386.h cmpintmd.h
 copy cmpauxmd\i386-nt.asm cmpauxmd.asm
 copy ntutl\*.lst .
 copy ntutl\*.h .
 copy ntutl\makefile .
+copy ntutl\setenv.bat .
