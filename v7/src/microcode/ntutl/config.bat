c87955436af2e0cebc0569ba5ad9671f45f33e32
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-08-21 03:20:16 +0000

    Move some files to ntutl.

--- v7/src/microcode/ntutl/config.bat
+++ v7/src/microcode/ntutl/config.bat
@@ -3,15 +3,17 @@ rem MIT Scheme microcode configuration script for Windows NT
 rem
 rem Copyright (c) 1993 Massachusetts Institute of Technology
 rem
-rem $Id: config.bat,v 1.4 1993/08/21 02:32:58 gjr Exp $
+rem $Id: config.bat,v 1.5 1993/08/21 03:20:16 gjr Exp $
 rem
 copy cmpintmd\i386.h cmpintmd.h
 copy cmpauxmd\i386-nt.asm cmpauxmd.asm
-copy ntutl\*.lst .
-copy ntutl\*.h .
 copy ntutl\makefile .
+copy ntutl\*.c .
+copy ntutl\*.h .
+copy ntutl\*.lst .
 copy ntutl\*.bat .
 copy ntutl\*.mak .
 copy ntutl\*.def .
 copy ntutl\*.dlg .
 copy ntutl\*.ico .
+copy ntutl\*.rc .
