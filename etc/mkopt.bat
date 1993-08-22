18677466a2f9e7ac18cbb3460547d5e8c4fe88cf
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-08-22 17:53:14 +0000

    Initial revision

---
+++ etc/mkopt.bat
@@ -0,0 +1,17 @@
+@echo off
+rem Copyright (c) 1993 Massachusetts Institute of Technology
+rem $Id: mkopt.bat,v 1.1 1993/08/22 17:52:59 gjr Exp $
+copy \scheme\runtime\numint.com \scheme\lib\options\numint.com
+copy \scheme\runtime\numint.bci \scheme\lib\options\numint.bci
+copy \scheme\runtime\cpress.com \scheme\lib\options\cpress.com
+copy \scheme\runtime\cpress.bci \scheme\lib\options\cpress.bci
+copy \scheme\runtime\dosproc.com \scheme\lib\options\dosproc.com
+copy \scheme\runtime\dosproc.bci \scheme\lib\options\dosproc.bci
+copy \scheme\runtime\format.com \scheme\lib\options\format.com
+copy \scheme\runtime\format.bci \scheme\lib\options\format.bci
+copy \scheme\runtime\hashtb.com \scheme\lib\options\hashtb.com
+copy \scheme\runtime\hashtb.bci \scheme\lib\options\hashtb.bci
+copy \scheme\runtime\krypt.com \scheme\lib\options\krypt.com
+copy \scheme\runtime\krypt.bci \scheme\lib\options\krypt.bci
+copy \scheme\runtime\process.com \scheme\lib\options\process.com
+copy \scheme\runtime\process.bci \scheme\lib\options\process.bci
