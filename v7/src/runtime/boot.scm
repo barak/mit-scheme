79862184405f307ce6e91df0b08ffd2c3738b6e7
Chris Hanson <org/chris-hanson/cph>
1993-08-31 08:42:34 +0000

    Undo random gratuitous changes.

--- v7/src/runtime/boot.scm
+++ v7/src/runtime/boot.scm
@@ -1,8 +1,8 @@
 #| -*-Scheme-*-
 
-$Id: boot.scm,v 14.8 1993/08/31 00:37:50 ziggy Exp $
+$Id: boot.scm,v 14.9 1993/08/31 08:42:34 cph Exp $
 
-Copyright (c) 1988-1993 Massachusetts Institute of Technology
+Copyright (c) 1988-93 Massachusetts Institute of Technology
 
 This material was developed by the Scheme project at the Massachusetts
 Institute of Technology, Department of Electrical Engineering and
@@ -62,29 +62,29 @@ MIT in each case. |#
   (and (procedure? object)
        (procedure-arity-valid? object 2)))
 
-(define-integrable interrupt-bit/stack      	#x0001)
-(define-integrable interrupt-bit/global-gc  	#x0002)
-(define-integrable interrupt-bit/gc         	#x0004)
-(define-integrable interrupt-bit/global-1   	#x0008)
-(define-integrable interrupt-bit/kbd        	#x0010)
-(define-integrable interrupt-bit/after-gc   	#x0020)
-(define-integrable interrupt-bit/timer      	#x0040)
-(define-integrable interrupt-bit/global-3   	#x0080)
-(define-integrable interrupt-bit/suspend    	#x0100)
+(define-integrable interrupt-bit/stack     #x0001)
+(define-integrable interrupt-bit/global-gc #x0002)
+(define-integrable interrupt-bit/gc        #x0004)
+(define-integrable interrupt-bit/global-1  #x0008)
+(define-integrable interrupt-bit/kbd       #x0010)
+(define-integrable interrupt-bit/after-gc  #x0020)
+(define-integrable interrupt-bit/timer     #x0040)
+(define-integrable interrupt-bit/global-3  #x0080)
+(define-integrable interrupt-bit/suspend   #x0100)
 ;; Interrupt bits #x0200 through #x4000 inclusive are reserved
 ;; for the Descartes PC sampler.
 
 ;; GC & stack overflow only
-(define-integrable interrupt-mask/gc-ok     	#x0007)
+(define-integrable interrupt-mask/gc-ok    #x0007)
 
 ;; GC, stack overflow, and timer only
-(define-integrable interrupt-mask/timer-ok	#x0047)
+(define-integrable interrupt-mask/timer-ok #x0047)
 
 ;; Absolutely everything off
-(define-integrable interrupt-mask/none      	#x0000)
+(define-integrable interrupt-mask/none     #x0000)
 
 ;; Normal: all enabled
-(define-integrable interrupt-mask/all       	#xFFFF)
+(define-integrable interrupt-mask/all      #xFFFF)
 
 (define (with-absolutely-no-interrupts thunk)
   (with-interrupt-mask interrupt-mask/none
