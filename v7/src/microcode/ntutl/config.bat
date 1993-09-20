8079bb5482f5f7cc91414a3745bdfe4cdacb770e
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-09-20 17:07:55 +0000

    Add .cur files.

--- v7/src/microcode/ntutl/config.bat
+++ v7/src/microcode/ntutl/config.bat
@@ -3,7 +3,7 @@ rem MIT Scheme microcode configuration script for Windows NT
 rem
 rem Copyright (c) 1993 Massachusetts Institute of Technology
 rem
-rem $Id: config.bat,v 1.5 1993/08/21 03:20:16 gjr Exp $
+rem $Id: config.bat,v 1.6 1993/09/20 17:07:36 gjr Exp $
 rem
 copy cmpintmd\i386.h cmpintmd.h
 copy cmpauxmd\i386-nt.asm cmpauxmd.asm
@@ -17,3 +17,4 @@ copy ntutl\*.def .
 copy ntutl\*.dlg .
 copy ntutl\*.ico .
 copy ntutl\*.rc .
+copy ntutl\*.cur .
