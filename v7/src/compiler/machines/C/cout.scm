c1d62aba096a49ac744dd5bb9d3cc0b2ff200fb5
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-11-09 04:27:38 +0000

    Fix data labels when timestamped.

--- v7/src/compiler/machines/C/cout.scm
+++ v7/src/compiler/machines/C/cout.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: cout.scm,v 1.11 1993/11/09 04:25:49 gjr Exp $
+$Id: cout.scm,v 1.12 1993/11/09 04:27:38 gjr Exp $
 
 Copyright (c) 1992-1993 Massachusetts Institute of Technology
 
@@ -114,12 +114,11 @@ MIT in each case. |#
 				   (car (last-pair dir))))
 			      "_"
 			      (canonicalize-label-name (pathname-name path))
-			      midfix
-			      suffix)))
+			      midfix)))
 	    (else
-	     (string-append default
+	     (string-append (canonicalize-label-name (pathname-name path))
 			    "_"
-			    (canonicalize-label-name (pathname-name path))
+			    default
 			    suffix
 			    time-stamp)))))
 
