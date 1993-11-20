41a096634f97f3e8b96bbd6864504f52f3fe39b4
Chris Hanson <org/chris-hanson/cph>
1993-11-20 07:41:08 +0000

    Update to reflect new directories.

--- v7/src/rcs/mklogs.scm
+++ v7/src/rcs/mklogs.scm
@@ -10,13 +10,16 @@
 			    (string-append subdirectory "/RCS"))
 			  subdirectories))))))
   (make-log "/scheme/src/6001")
-  (make-log "/scheme/src/compiler" "back" "base" "fggen" "fgopt"
-	    "rtlbase" "rtlgen" "rtlopt" "machines/bobcat" "machines/i386"
-	    "machines/mips" "machines/spectrum" "machines/vax")
+  (make-log "/scheme/src/compiler" "back" "base" "documentation" "etc" "fggen"
+	    "fgopt" "machines/C" "machines/alpha" "machines/bobcat"
+	    "machines/i386" "machines/mips" "machines/sparc"
+	    "machines/spectrum" "machines/vax" "rtlbase" "rtlgen" "rtlopt")
   (make-log "/scheme/src/cref")
   (make-log "/scheme/src/edwin")
-  (make-log "/scheme/src/microcode" "m" "s")
+  (make-log "/scheme/src/microcode" "cmpauxmd" "cmpintmd" "dosutl" "m" "ntutl"
+	    "s" "unxutl")
   (make-log "/scheme/src/rcs")
   (make-log "/scheme/src/runtime")
   (make-log "/scheme/src/sf")
-  (make-log "/scheme/src/sicp"))
+  (make-log "/scheme/src/sicp")
+  (make-log "/scheme/src/win32" "dibutils"))
