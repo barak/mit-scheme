bc16520579116a927aca46cac7d505e2f6f92557
Guillermo J. Rozas <edu/mit/csail/zurich/gjr>
1993-11-29 19:11:24 +0000

    Fix minor bug.

--- v7/src/compiler/base/toplev.scm
+++ v7/src/compiler/base/toplev.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: toplev.scm,v 4.51 1993/11/29 19:10:21 gjr Exp $
+$Id: toplev.scm,v 4.52 1993/11/29 19:11:24 gjr Exp $
 
 Copyright (c) 1988-1993 Massachusetts Institute of Technology
 
@@ -149,7 +149,7 @@ MIT in each case. |#
 (define (compile-procedure procedure #!optional keep-debugging-info?)
   (compiler-output->procedure
    (compile-scode/no-file
-    (procedure-lambda scode)
+    (procedure-lambda procedure)
     (and (or (default-object? keep-debugging-info?)
 	     keep-debugging-info?)
 	 'KEEP))
