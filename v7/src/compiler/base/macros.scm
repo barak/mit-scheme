005a6c04c38eeddcc8050a6385e168c24458b1e5
Stephen Adams <edu/mit/csail/zurich/adams>
1995-04-24 22:30:44 +0000

    Fixed TRANSFORM/PACKAGE to always return UNSPECIFIC, which ensures that the last thing in the generated LET is not a definition.

--- v7/src/compiler/base/macros.scm
+++ v7/src/compiler/base/macros.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: macros.scm,v 4.13 1993/07/01 03:09:43 gjr Exp $
+$Id: macros.scm,v 4.14 1995/04/24 22:30:44 adams Exp $
 
 Copyright (c) 1988-1993 Massachusetts Institute of Technology
 
@@ -95,7 +95,7 @@ MIT in each case. |#
 	       (make-definition name (make-unassigned-reference-trap)))
 	     names)
       ,(make-combination
-	(let ((block (syntax* body)))
+	(let ((block (syntax* (append body unspecific))))
 	  (if (open-block? block)
 	      (open-block-components block
 		(lambda (names* declarations body)
