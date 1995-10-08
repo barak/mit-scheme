080d12f882cafd593199b57b8f6721295fcfabcc
Chris Hanson <org/chris-hanson/cph>
1995-10-08 15:23:41 +0000

    Test __IBMC__ in addition to __STDC__ because the IBM OS/2 C compiler defines __STDC__ to mean "strict ANSI" rather than "ANSI features supported".

--- v7/src/microcode/cmpint.c
+++ v7/src/microcode/cmpint.c
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: cmpint.c,v 1.85 1995/10/05 03:27:29 cph Exp $
+$Id: cmpint.c,v 1.86 1995/10/08 15:23:41 cph Exp $
 
 Copyright (c) 1989-95 Massachusetts Institute of Technology
 
@@ -139,7 +139,7 @@ MIT in each case. */
 #define ASM_ENTRY_POINT(name) name
 #endif
 
-#ifdef __STDC__
+#if defined(__STDC__) || defined(__IBMC__)
 #define EXFNX(name, proto) ASM_ENTRY_POINT (name) proto
 #define DEFNX(name, arglist, args) ASM_ENTRY_POINT (name) (args)
 #define DEFNX_VOID(name) ASM_ENTRY_POINT (name) (void)
