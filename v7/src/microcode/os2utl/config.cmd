45173ab731071db6c9fb7f73b6005015b10b50bb
Chris Hanson <org/chris-hanson/cph>
1995-10-09 05:58:55 +0000

    Reorganize makefile to allow it to be easily configured for different compilers.  Now supports IBM Visual Age C++, IBM C Set++/2, EMX/GCC, and GCC/2.  Watcom support will be added shortly.

--- v7/src/microcode/os2utl/config.cmd
+++ v7/src/microcode/os2utl/config.cmd
@@ -3,8 +3,9 @@ rem MIT Scheme microcode configuration script for OS/2
 rem
 rem Copyright (c) 1994 Massachusetts Institute of Technology
 rem
-rem $Id: config.cmd,v 1.1 1994/11/28 05:26:34 cph Exp $
+rem $Id: config.cmd,v 1.2 1995/10/09 05:58:38 cph Exp $
 rem
 copy cmpintmd\i386.h cmpintmd.h
-copy cmpauxmd\i386-os2.asm cmpauxmd.asm
+copy cmpauxmd\i386.m4 cmpauxmd.m4
 copy os2utl\makefile .
+echo ***** Read and edit the makefile! *****
