d74cb69dd1c35a7a029522781755a1f746d564f1
Chris Hanson <org/chris-hanson/cph>
1995-11-12 05:46:34 +0000

    Update with current directories, and also to use new calling conventions for RCS-DIRECTORY-LOG.

--- v7/src/rcs/mklogs.scm
+++ v7/src/rcs/mklogs.scm
@@ -1,17 +1,51 @@
-;;; -*-Scheme-*-
-(let ((rcs-directory-log (access rcs-directory-log (->environment '(RCS)))))
-  (for-each (lambda (directory)
-	      (rcs-directory-log
-	       (merge-pathnames "RCS.log"
-				(pathname-as-directory directory))
-	       directory))
-	    '("/scheme/src/6001"
-	      "/scheme/src/compiler"
-	      "/scheme/src/cref"
-	      "/scheme/src/edwin"
-	      "/scheme/src/microcode"
-	      "/scheme/src/rcs"
-	      "/scheme/src/runtime"
-	      "/scheme/src/sf"
-	      "/scheme/src/sicp"
-	      "/scheme/src/win32")))
+#| -*-Scheme-*-
+
+$Id: mklogs.scm,v 1.7 1995/11/12 05:46:34 cph Exp $
+
+Copyright (c) 1988-95 Massachusetts Institute of Technology
+
+This material was developed by the Scheme project at the Massachusetts
+Institute of Technology, Department of Electrical Engineering and
+Computer Science.  Permission to copy this software, to redistribute
+it, and to use it for any purpose is granted, subject to the following
+restrictions and understandings.
+
+1. Any copy made of this software must include this copyright notice
+in full.
+
+2. Users of this software agree to make their best efforts (a) to
+return to the MIT Scheme project any improvements or extensions that
+they make, so that these may be included in future releases; and (b)
+to inform MIT of noteworthy uses of this software.
+
+3. All materials developed as a consequence of the use of this
+software shall duly acknowledge such use, in accordance with the usual
+standards of acknowledging credit in academic research.
+
+4. MIT has made no warrantee or representation that the operation of
+this software will be error-free, and MIT is under no obligation to
+provide any services, by way of maintenance, update, or otherwise.
+
+5. In conjunction with products arising from the use of this material,
+there shall be no use of the name of the Massachusetts Institute of
+Technology nor of any adaptation thereof in any advertising,
+promotional, or sales literature without prior written consent from
+MIT in each case. |#
+
+;;;; Update the RCS log files in the standard Scheme directories.
+
+(for-each rcs-directory-log
+	  '("/scheme/7.4/src/compiler"
+	    "/scheme/7.4/src/microcode"
+	    "/scheme/7.4/src/runtime"
+	    "/scheme/7.4/src/sf"
+	    "/scheme/8.0/src/6001"
+	    "/scheme/8.0/src/bench"
+	    "/scheme/8.0/src/compiler"
+	    "/scheme/8.0/src/cref"
+	    "/scheme/8.0/src/edwin"
+	    "/scheme/8.0/src/microcode"
+	    "/scheme/8.0/src/rcs"
+	    "/scheme/8.0/src/runtime"
+	    "/scheme/8.0/src/sf"
+	    "/scheme/8.0/src/win32"))
