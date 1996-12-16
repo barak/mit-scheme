b694e6570a11abc61916fc1df0efe1b6d63e489a
Chris Hanson <org/chris-hanson/cph>
1996-12-16 04:53:17 +0000

    Use special allocator to allocate the heap when running under Linux. Linux ELF pushes the text and data segments up into high addresses, leaving most of the low addresses empty.  To get access to them, we must use mmap.

--- v7/src/microcode/config.h
+++ v7/src/microcode/config.h
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: config.h,v 9.96 1996/12/16 04:37:14 cph Exp $
+$Id: config.h,v 9.97 1996/12/16 04:53:17 cph Exp $
 
 Copyright (c) 1987-96 Massachusetts Institute of Technology
 
@@ -443,6 +443,7 @@ typedef unsigned long SCHEME_OBJECT;
 #ifdef __linux
 extern void * linux_heap_malloc (unsigned long);
 #define HEAP_MALLOC linux_heap_malloc
+#define HEAP_FREE(address)
 #endif
 
 #if defined(WINNT) && !defined(WINNT_RAW_ADDRESSES)
