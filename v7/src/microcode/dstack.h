d0e1227ea86510e75ce5c783bbea3fa0c052b295
Chris Hanson <org/chris-hanson/cph>
1999-01-03 05:34:02 +0000

    Fix mis-attributed copyright statement.  These files were written by me but attributed to FSF when they should have been attributed to MIT.

--- v7/src/microcode/dstack.h
+++ v7/src/microcode/dstack.h
@@ -1,20 +1,23 @@
-/* Copyright (C) 1990-92 Free Software Foundation, Inc.
+/* -*-C-*-
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 1, or (at your option)
-   any later version.
+$Id: dstack.h,v 1.9 1999/01/03 05:33:41 cph Exp $
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+Copyright (C) 1990-1999 Massachusetts Institute of Technology
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
-/* $Id: dstack.h,v 1.8 1993/10/27 23:54:39 gjr Exp $ */
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #ifndef __DSTACK_H__
 #define __DSTACK_H__
--- v7/src/microcode/ptrvec.c
+++ v7/src/microcode/ptrvec.c
@@ -1,20 +1,23 @@
-/* Copyright (C) 1990 Free Software Foundation, Inc.
+/* -*-C-*-
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 1, or (at your option)
-   any later version.
+$Id: ptrvec.c,v 1.3 1999/01/03 05:33:56 cph Exp $
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+Copyright (C) 1990-1999 Massachusetts Institute of Technology
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
-/* $Id: ptrvec.c,v 1.2 1993/06/24 06:17:08 gjr Exp $ */
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #include "outf.h"
 #include "dstack.h"
