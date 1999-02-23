a2a56b48bc58b1cf40f00931a1e737fa5c108300
Chris Hanson <org/chris-hanson/cph>
1999-02-23 21:34:01 +0000

    When user aborts a computation, deregister all outstanding thread events.

--- v7/src/runtime/rep.scm
+++ v7/src/runtime/rep.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: rep.scm,v 14.54 1999/02/23 21:32:22 cph Exp $
+$Id: rep.scm,v 14.55 1999/02/23 21:34:01 cph Exp $
 
 Copyright (c) 1988-1999 Massachusetts Institute of Technology
 
@@ -142,7 +142,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 		 (loop
 		  (bind-abort-restart cmdl
 		    (lambda ()
-		      (deregister-thread-events)
+		      (deregister-all-events)
 		      (with-interrupt-mask interrupt-mask/all
 			(lambda (interrupt-mask)
 			  interrupt-mask
