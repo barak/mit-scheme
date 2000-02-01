74e39d034a7692fd47205e612f9f6d5c7205dd1a
Chris Hanson <org/chris-hanson/cph>
2000-02-01 01:50:18 +0000

    Update version number to mark changes made to support FreeBSD.

--- v7/src/microcode/version.h
+++ v7/src/microcode/version.h
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: version.h,v 11.168 2000/01/30 01:03:31 cph Exp $
+$Id: version.h,v 11.169 2000/02/01 01:50:18 cph Exp $
 
 Copyright (c) 1988-2000 Massachusetts Institute of Technology
 
@@ -24,7 +24,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 /* Scheme system release version */
 
 #ifndef RELEASE
-#define RELEASE		"7.5.1"
+#define RELEASE		"7.5.2"
 #endif
 
 /* Microcode release version */
@@ -33,5 +33,5 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #define VERSION		11
 #endif
 #ifndef SUBVERSION
-#define SUBVERSION	165
+#define SUBVERSION	166
 #endif
