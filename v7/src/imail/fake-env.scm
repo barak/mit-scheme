5a8d4076e5f8131ac0126e167e88b9f5b317a34f
Chris Hanson <org/chris-hanson/cph>
2000-04-18 21:48:31 +0000

    Initial registration.

---
+++ v7/src/imail/fake-env.scm
@@ -0,0 +1,13 @@
+(let ((new-child
+       (lambda (parent name)
+	 (let ((package (name->package parent)))
+	   (package/add-child! package
+			       name
+			       (in-package (package/environment package)
+				 (make-environment)))))))
+  (new-child '(EDWIN) 'IMAIL)
+  (new-child '(EDWIN IMAIL) 'IMAP-SYNTAX)
+  (new-child '(EDWIN IMAIL) 'PARSER)
+  (new-child '(EDWIN IMAIL) 'REXP)
+  (new-child '(EDWIN IMAIL) 'RFC822)
+  (new-child '(EDWIN IMAIL) 'URL))
