33e71f7acfbc2f6a6e91ea2da70cd57a46f4cfc2
Chris Hanson <org/chris-hanson/cph>
2000-06-02 02:33:56 +0000

    Move Edwin variable exports back to "imail.pkg".  Add subsystem ID string for IMAIL.

--- v7/src/imail/load.scm
+++ v7/src/imail/load.scm
@@ -1,6 +1,6 @@
 ;;; -*-Scheme-*-
 ;;;
-;;; $Id: load.scm,v 1.13 2000/06/01 20:10:21 cph Exp $
+;;; $Id: load.scm,v 1.14 2000/06/02 02:33:56 cph Exp $
 ;;;
 ;;; Copyright (c) 1999-2000 Massachusetts Institute of Technology
 ;;;
@@ -27,74 +27,4 @@
   (lambda ()
     (fluid-let ((*allow-package-redefinition?* #t))
       (package/system-loader "imail" '() 'QUERY))))
-
-;; Kludge necessary to allow reloading of this package.
-(for-each (let ((from (->environment '(EDWIN IMAIL)))
-		(to (->environment '(EDWIN))))
-	    (lambda (name)
-	      (if (lexical-unreferenceable? to name)
-		  (environment-link-name to from name)
-		  (lexical-assignment to name (lexical-reference from name)))))
-	  '(edwin-command$imail
-	    edwin-command$imail-add-flag
-	    edwin-command$imail-continue
-	    edwin-command$imail-copy-messages
-	    edwin-command$imail-create-folder
-	    edwin-command$imail-delete-backward
-	    edwin-command$imail-delete-folder
-	    edwin-command$imail-delete-forward
-	    edwin-command$imail-delete-message
-	    edwin-command$imail-expunge
-	    edwin-command$imail-first-message
-	    edwin-command$imail-forward
-	    edwin-command$imail-get-new-mail
-	    edwin-command$imail-input
-	    edwin-command$imail-input-from-folder
-	    edwin-command$imail-kill-flag
-	    edwin-command$imail-last-message
-	    edwin-command$imail-mail
-	    edwin-command$imail-next-flagged-message
-	    edwin-command$imail-next-message
-	    edwin-command$imail-next-undeleted-message
-	    edwin-command$imail-output
-	    edwin-command$imail-previous-flagged-message
-	    edwin-command$imail-previous-message
-	    edwin-command$imail-previous-undeleted-message
-	    edwin-command$imail-quit
-	    edwin-command$imail-reply
-	    edwin-command$imail-resend
-	    edwin-command$imail-save-folder
-	    edwin-command$imail-search
-	    edwin-command$imail-select-message
-	    edwin-command$imail-summary
-	    edwin-command$imail-summary-by-flags
-	    edwin-command$imail-summary-by-recipients
-	    edwin-command$imail-summary-quit
-	    edwin-command$imail-summary-select-message
-	    edwin-command$imail-toggle-header
-	    edwin-command$imail-undelete-backward
-	    edwin-command$imail-undelete-forward
-	    edwin-command$imail-undelete-previous-message
-	    edwin-mode$imail
-	    edwin-mode$imail-summary
-	    edwin-variable$imail-default-dont-reply-to-names
-	    edwin-variable$imail-default-imap-mailbox
-	    edwin-variable$imail-default-imap-server
-	    edwin-variable$imail-default-user-id
-	    edwin-variable$imail-delete-after-output
-	    edwin-variable$imail-dont-reply-to-names
-	    edwin-variable$imail-expunge-confirmation
-	    edwin-variable$imail-ignored-headers
-	    edwin-variable$imail-kept-headers
-	    edwin-variable$imail-message-filter
-	    edwin-variable$imail-mode-hook
-	    edwin-variable$imail-pass-phrase-retention-time
-	    edwin-variable$imail-primary-folder
-	    edwin-variable$imail-receive-mime
-	    edwin-variable$imail-reply-with-re
-	    edwin-variable$imail-summary-highlight-message
-	    edwin-variable$imail-summary-mode-hook
-	    edwin-variable$imail-summary-pop-up-message
-	    edwin-variable$imail-summary-show-date
-	    edwin-variable$imail-summary-subject-width
-	    edwin-variable$imail-update-interval))
+(add-subsystem-identification! "IMAIL" '(1 1))
