216ff1d2aa4d53683d6d304e4a3b800b3507e9d4
Chris Hanson <org/chris-hanson/cph>
2000-08-18 19:32:02 +0000

    Add imail.

--- etc/build-tree.sh
+++ etc/build-tree.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: build-tree.sh,v 1.3 2000/03/22 01:52:34 cph Exp $
+# $Id: build-tree.sh,v 1.4 2000/08/18 19:32:02 cph Exp $
 #
 # Program to finish setting up the Scheme source tree after it is
 # checked out.  Adds required links, builds TAGS files, etc.
@@ -14,11 +14,11 @@ for directory in 6001 cref edwin rcs runtime sf sos win32
 do
   (cd $directory; ln -s ../Makefile.std Makefile)
 done
-for directory in 6001 compiler cref edwin rcs runtime sf sos swat win32
+for directory in 6001 compiler cref edwin imail rcs runtime sf sos swat win32
 do
   (cd $directory; make TAGS)
 done
-for directory in edwin runtime sos
+for directory in edwin imail runtime sos
 do
   (cd $directory; ln -s ed-ffi.scm .edwin-ffi)
 done
@@ -27,7 +27,7 @@ done
 (cd microcode/cmpauxmd; make all)
 (cd pcsample; etags *.scm *.c)
 (cd compiler/machines/vax;
- for n in 1 2 3
- do
-   ln -s instr${n}.scm dinstr${n}.scm
- done)
+  for n in 1 2 3
+  do
+    ln -s instr${n}.scm dinstr${n}.scm
+  done)
