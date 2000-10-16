6afcd3472e6bf0045e76d912e21424d780230217
Chris Hanson <org/chris-hanson/cph>
2000-10-16 18:02:42 +0000

    Add copyright notice.

--- v7/dist/optiondb.scm
+++ v7/dist/optiondb.scm
@@ -1,4 +1,23 @@
-;; -*- Scheme -*-
+#| -*-Scheme-*-
+
+$Id: optiondb.scm,v 1.4 2000/10/16 17:58:27 cph Exp $
+
+Copyright (c) 2000 Massachusetts Institute of Technology
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+|#
 
 (define (guarded-system-loader package-name place #!optional filename)
   (let ((directory
--- v7/src/etc/build-tree.sh
+++ v7/src/etc/build-tree.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 #
-# $Id: build-tree.sh,v 1.1 2000/10/16 18:01:52 cph Exp $
+# $Id: build-tree.sh,v 1.2 2000/10/16 18:02:42 cph Exp $
 #
+# Copyright (c) 2000 Massachusetts Institute of Technology
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 # Program to finish setting up the Scheme source tree after it is
 # checked out.  Adds required links, builds TAGS files, etc.
-#
+
 if [ ! -d 6001 ]
 then
   echo "This must be run from the top-level Scheme source directory."
