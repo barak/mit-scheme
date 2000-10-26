2dd034b26644edc27d0a430298a7a06ce890d916
Chris Hanson <org/chris-hanson/cph>
2000-10-26 04:36:47 +0000

    Fix thinko.

--- v7/src/edwin/wincom.scm
+++ v7/src/edwin/wincom.scm
@@ -1,6 +1,6 @@
 ;;; -*-Scheme-*-
 ;;;
-;;; $Id: wincom.scm,v 1.128 2000/10/26 04:24:20 cph Exp $
+;;; $Id: wincom.scm,v 1.129 2000/10/26 04:36:47 cph Exp $
 ;;;
 ;;; Copyright (c) 1987, 1989-2000 Massachusetts Institute of Technology
 ;;;
@@ -545,7 +545,8 @@ Also kills any pop up window it may have created."
 		(set-variable! split-height-threshold limit))
 	    (cond ((and (use-multiple-screens?)
 			(other-screen screen 1 #t))
-		   => (lambda (screen) (pop-into-window selected)))
+		   => (lambda (screen)
+			(pop-into-window (screen-selected-window screen))))
 		  ((ref-variable preserve-window-arrangement)
 		   (pop-into-window (largest-window screen)))
 		  ((not (ref-variable pop-up-windows))
