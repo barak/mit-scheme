cfad1e65994523e57cbb2f805f0d7efd4b485a2f
Chris Hanson <org/chris-hanson/cph>
2000-12-06 05:52:40 +0000

    Move knowledge about doing "maintainer-clean" into script in microcode directory.

---
+++ v7/src/microcode/Mclean.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# $Id: Mclean.sh,v 1.1 2000/12/06 05:52:40 cph Exp $
+#
+# Copyright (c) 2000 Massachusetts Institute of Technology
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# Program to do "maintainer-clean" rule.
+
+test ! -f Makefile && test -f configure && ./configure
+make maintainer-clean
