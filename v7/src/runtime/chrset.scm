113c49f359475d8d490830767bc5d66c8f882e56
Chris Hanson <org/chris-hanson/cph>
2001-02-05 18:33:19 +0000

    Add inversions for all standard character sets.

--- v7/src/runtime/chrset.scm
+++ v7/src/runtime/chrset.scm
@@ -1,8 +1,8 @@
 #| -*-Scheme-*-
 
-$Id: chrset.scm,v 14.10 2000/04/12 00:37:20 cph Exp $
+$Id: chrset.scm,v 14.11 2001/02/05 18:33:13 cph Exp $
 
-Copyright (c) 1988-2000 Massachusetts Institute of Technology
+Copyright (c) 1988-2001 Massachusetts Institute of Technology
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -118,31 +118,47 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 (define char-set:lower-case)
 (define char-set:numeric)
 (define char-set:graphic)
-(define char-set:not-graphic)
 (define char-set:whitespace)
-(define char-set:not-whitespace)
 (define char-set:alphabetic)
 (define char-set:alphanumeric)
 (define char-set:standard)
+
+(define char-set:not-upper-case)
+(define char-set:not-lower-case)
+(define char-set:not-numeric)
+(define char-set:not-graphic)
+(define char-set:not-whitespace)
+(define char-set:not-alphabetic)
+(define char-set:not-alphanumeric)
+(define char-set:not-standard)
+
 (define char-set:not-01)
 (define char-set:newline)
 
 (define (initialize-package!)
-  (set! char-set:not-01 (ascii-range->char-set #x02 #x100))
   (set! char-set:upper-case (ascii-range->char-set #x41 #x5B))
   (set! char-set:lower-case (ascii-range->char-set #x61 #x7B))
   (set! char-set:numeric (ascii-range->char-set #x30 #x3A))
   (set! char-set:graphic (ascii-range->char-set #x20 #x7F))
-  (set! char-set:not-graphic (char-set-invert char-set:graphic))
   (set! char-set:whitespace
 	(char-set char:newline #\Tab #\Linefeed #\Page #\Return #\Space))
-  (set! char-set:not-whitespace (char-set-invert char-set:whitespace))
   (set! char-set:alphabetic
 	(char-set-union char-set:upper-case char-set:lower-case))
   (set! char-set:alphanumeric
 	(char-set-union char-set:alphabetic char-set:numeric))
   (set! char-set:standard
 	(char-set-union char-set:graphic (char-set char:newline)))
+
+  (set! char-set:not-upper-case (char-set-invert char-set:upper-case))
+  (set! char-set:not-lower-case (char-set-invert char-set:lower-case))
+  (set! char-set:not-numeric (char-set-invert char-set:numeric))
+  (set! char-set:not-graphic (char-set-invert char-set:graphic))
+  (set! char-set:not-whitespace (char-set-invert char-set:whitespace))
+  (set! char-set:not-alphabetic (char-set-invert char-set:alphabetic))
+  (set! char-set:not-alphanumeric (char-set-invert char-set:alphanumeric))
+  (set! char-set:not-standard (char-set-invert char-set:standard))
+
+  (set! char-set:not-01 (ascii-range->char-set #x02 #x100))
   (set! char-set:newline (char-set char:newline))
   unspecific)
 
