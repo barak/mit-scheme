c728695b103cdb5ba6e4c52b0c5a6f53f71c7eba
Chris Hanson <org/chris-hanson/cph>
2001-06-15 20:36:18 +0000

    Don't integrate the primitives SUBSTRING-FIND-NEXT-CHAR-IN-SET and SUBSTRING-FIND-PREVIOUS-CHAR-IN-SET.

--- v7/src/sf/gconst.scm
+++ v7/src/sf/gconst.scm
@@ -1,8 +1,8 @@
 #| -*-Scheme-*-
 
-$Id: gconst.scm,v 4.23 1999/01/02 06:06:43 cph Exp $
+$Id: gconst.scm,v 4.24 2001/06/15 20:36:18 cph Exp $
 
-Copyright (c) 1987-1999 Massachusetts Institute of Technology
+Copyright (c) 1987-2001 Massachusetts Institute of Technology
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,7 +16,8 @@ General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.
 |#
 
 ;;;; SCode Optimizer: Global Constants List
@@ -222,8 +223,6 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
     STRING?
     SUBSTRING-CI=?
     SUBSTRING-DOWNCASE!
-    SUBSTRING-FIND-NEXT-CHAR-IN-SET
-    SUBSTRING-FIND-PREVIOUS-CHAR-IN-SET
     SUBSTRING-MATCH-BACKWARD
     SUBSTRING-MATCH-BACKWARD-CI
     SUBSTRING-MATCH-FORWARD
