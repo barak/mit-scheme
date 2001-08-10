32d885acb90661c55b2208e00b8aff5b27934c20
Chris Hanson <org/chris-hanson/cph>
2001-08-10 17:27:52 +0000

    Fix bug: "${S}/*" -> "${S}"/*.

--- v7/src/compiler/Stage.sh
+++ v7/src/compiler/Stage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $Id: Stage.sh,v 1.1 2000/12/08 05:53:02 cph Exp $
+# $Id: Stage.sh,v 1.2 2001/08/10 17:27:52 cph Exp $
 #
-# Copyright (c) 2000 Massachusetts Institute of Technology
+# Copyright (c) 2000, 2001 Massachusetts Institute of Technology
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
 
 # Utility for MIT Scheme compiler staging.
 
@@ -36,7 +37,7 @@ make)
     ;;
 unmake)
     for D in ${SUBDIRS}; do
-	(cd ${D} && mv -f "${S}/*" . && rmdir "${S}") || exit 1
+	(cd ${D} && mv -f "${S}"/* . && rmdir "${S}") || exit 1
     done
     ;;
 remove)
@@ -46,12 +47,12 @@ remove)
     ;;
 copy)
     for D in ${SUBDIRS}; do
-	(cd ${D} && cp "${S}/*" .) || exit 1
+	(cd ${D} && cp "${S}"/* .) || exit 1
     done
     ;;
 link)
     for D in ${SUBDIRS}; do
-	(cd ${D} && ln "${S}/*" .) || exit 1
+	(cd ${D} && ln "${S}"/* .) || exit 1
     done
     ;;
 *)
