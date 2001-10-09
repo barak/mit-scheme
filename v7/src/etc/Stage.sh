3a439d4496eebe9f8426b957ca7600cf466d0b2e
Chris Hanson <org/chris-hanson/cph>
2001-10-09 21:17:25 +0000

    Fix quoting problem.

--- v7/src/etc/Stage.sh
+++ v7/src/etc/Stage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $Id: Stage.sh,v 1.2 2000/12/08 05:48:56 cph Exp $
+# $Id: Stage.sh,v 1.3 2001/10/09 21:17:25 cph Exp $
 #
-# Copyright (c) 2000 Massachusetts Institute of Technology
+# Copyright (c) 2000, 2001 Massachusetts Institute of Technology
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
 
 # Utility for MIT Scheme compiler staging.
 
@@ -32,16 +33,16 @@ make)
     mkdir "${DIRNAME}" && mv -f *.com *.bci "${DIRNAME}/."
     ;;
 unmake)
-    mv -f "${DIRNAME}/*" . && rmdir "${DIRNAME}"
+    mv -f "${DIRNAME}"/* . && rmdir "${DIRNAME}"
     ;;
 remove)
     rm -rf "${DIRNAME}"
     ;;
 copy)
-    cp "${DIRNAME}/*" .
+    cp "${DIRNAME}"/* .
     ;;
 link)
-    ln "${DIRNAME}/*" .
+    ln "${DIRNAME}"/* .
     ;;
 *)
     echo "$0: Unknown command ${1}"
