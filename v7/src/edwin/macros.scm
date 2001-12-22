fcad19d4ceb264a5999c5b36f8898919824b0b69
Chris Hanson <org/chris-hanson/cph>
2001-12-22 04:00:39 +0000

    Store macro definitions in environments rather than in syntax tables.

--- v7/src/edwin/macros.scm
+++ v7/src/edwin/macros.scm
@@ -1,8 +1,8 @@
 ;;; -*-Scheme-*-
 ;;;
-;;; $Id: macros.scm,v 1.68 2001/12/21 18:41:22 cph Exp $
+;;; $Id: macros.scm,v 1.69 2001/12/22 04:00:39 cph Exp $
 ;;;
-;;; Copyright (c) 1986, 1989-1999 Massachusetts Institute of Technology
+;;; Copyright (c) 1986, 1989-1999, 2001 Massachusetts Institute of Technology
 ;;;
 ;;; This program is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU General Public License as
@@ -16,7 +16,8 @@
 ;;;
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program; if not, write to the Free Software
-;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;;; 02111-1307, USA.
 
 ;;;; Editor Macros
 
