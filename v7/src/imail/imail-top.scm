3a165bfc1e9b9b223a678daa2532749bfba8b6ed
Chris Hanson <org/chris-hanson/cph>
2003-01-15 21:26:02 +0000

    Change default value of imail-mime-collapse-digest to #f.

--- v7/src/imail/imail-top.scm
+++ v7/src/imail/imail-top.scm
@@ -1,25 +1,26 @@
-;;; -*-Scheme-*-
-;;;
-;;; $Id: imail-top.scm,v 1.284 2002/11/20 19:46:05 cph Exp $
-;;;
-;;; Copyright (c) 1999-2002 Massachusetts Institute of Technology
-;;;
-;;; This file is part of MIT Scheme.
-;;;
-;;; MIT Scheme is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published
-;;; by the Free Software Foundation; either version 2 of the License,
-;;; or (at your option) any later version.
-;;;
-;;; MIT Scheme is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with MIT Scheme; if not, write to the Free Software
-;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-;;; 02111-1307, USA.
+#| -*-Scheme-*-
+
+$Id: imail-top.scm,v 1.285 2003/01/15 21:26:02 cph Exp $
+
+Copyright 1999,2000,2001,2002,2003 Massachusetts Institute of Technology
+
+This file is part of MIT Scheme.
+
+MIT Scheme is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+MIT Scheme is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MIT Scheme; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+|#
 
 ;;;; IMAIL mail reader: top level
 
@@ -192,7 +193,7 @@ Otherwise, only one of the parts is shown."
 
 (define-variable imail-mime-collapse-digest
   "If true, component messages of a MIME digest are shown as attachments."
-  #t
+  #f
   boolean?)
 
 (define-variable imail-mime-boundary-style
