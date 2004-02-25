f0524b50f15372b03978a998285970b0139f1bf6
Chris Hanson <org/chris-hanson/cph>
2004-02-25 20:59:02 +0000

    Add name for BOM character.

--- v7/src/runtime/char.scm
+++ v7/src/runtime/char.scm
@@ -1,9 +1,9 @@
 #| -*-Scheme-*-
 
-$Id: char.scm,v 14.24 2004/01/16 05:44:21 cph Exp $
+$Id: char.scm,v 14.25 2004/02/25 20:59:02 cph Exp $
 
 Copyright 1986,1987,1988,1991,1995,1997 Massachusetts Institute of Technology
-Copyright 1998,2001,2003 Massachusetts Institute of Technology
+Copyright 1998,2001,2003,2004 Massachusetts Institute of Technology
 
 This file is part of MIT/GNU Scheme.
 
@@ -350,7 +350,8 @@ USA.
     (#x1F #f "us" "backnext")
     (#x20 "space" "spc" "sp")
     (#x7F "delete" "del" "rubout")
-    (#xA0 "nbsp")))
+    (#xA0 "nbsp")
+    (#xFEFF "bom")))
 
 (define named-bits
   '((#x01 "M" "meta")
