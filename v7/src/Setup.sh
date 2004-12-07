733b05e050283c8873a424f40008b947c2bca0e9
Chris Hanson <org/chris-hanson/cph>
2004-12-07 04:29:58 +0000

    Add "xdoc" to directory list.

--- v7/src/Setup.sh
+++ v7/src/Setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $Id: Setup.sh,v 1.11 2003/12/29 07:32:55 uid67408 Exp $
+# $Id: Setup.sh,v 1.12 2004/12/07 04:29:58 cph Exp $
 #
-# Copyright 2000,2001,2003 Massachusetts Institute of Technology
+# Copyright 2000,2001,2003,2004 Massachusetts Institute of Technology
 #
 # This file is part of MIT/GNU Scheme.
 #
@@ -45,10 +45,8 @@ maybe_link lib/edwin/etc/TUTORIAL ../../../etc/TUTORIAL
 maybe_link lib/edwin/etc/mime.types ../../../etc/mime.types
 maybe_link lib/edwin/autoload ../../edwin
 
-SUBDIRS=""
-
 for SUBDIR in 6001 compiler cref edwin imail microcode rcs \
-	runtime runtime-check sf sos ssp star-parser win32 xml; do
+	runtime runtime-check sf sos ssp star-parser win32 xdoc xml; do
     echo "setting up ${SUBDIR}"
     maybe_link ${SUBDIR}/Setup.sh ../etc/Setup.sh
     ( cd ${SUBDIR} && ./Setup.sh ) || exit 1
