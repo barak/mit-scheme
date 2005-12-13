a3687c1f52c2a9b41e7a2852dffaff3ad64bbe3f
Chris Hanson <org/chris-hanson/cph>
2005-12-13 06:41:05 +0000

    Ugh: the CVS/Root file for this directory was not updated after the move to Savannah, and consequently all of the updates have been recorded in the wrong place.  Merge them back in now.

--- v7/dist/copyright.c
+++ v7/dist/copyright.c
@@ -1,23 +1,24 @@
 /* -*-C-*-
 
-$Id: copyright.c,v 1.3 2002/11/20 18:32:26 cph Exp $
+$Id: copyright.c,v 1.4 2005/12/13 06:40:58 cph Exp $
 
-Copyright 2002 Massachusetts Institute of Technology
+Copyright 2005 Massachusetts Institute of Technology
 
-This file is part of MIT Scheme.
+This file is part of MIT/GNU Scheme.
 
-MIT Scheme is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
+MIT/GNU Scheme is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
-MIT Scheme is distributed in the hope that it will be useful, but
+MIT/GNU Scheme is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with MIT Scheme; if not, write to the Free Software Foundation,
-Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+along with MIT/GNU Scheme; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+USA.
 
 */
