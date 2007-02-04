55499b4fd07282641ba84ddda603ccf19fd14aed
Taylor R. Campbell <net/mumble/campbell>
2007-02-04 18:36:12 +0000

    Eliminate compiler warning about non-return from non-void function x_close_window_io_error, which calls longjmp instead.

--- v7/src/microcode/x11base.c
+++ v7/src/microcode/x11base.c
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: x11base.c,v 1.91 2007/01/12 03:45:55 cph Exp $
+$Id: x11base.c,v 1.92 2007/02/04 18:36:12 riastradh Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -641,6 +641,8 @@ static int
 DEFUN (x_close_window_io_error, (display), Display * display)
 {
   longjmp (x_close_window_jmp_buf, 1);
+  /*NOTREACHED*/
+  return (0);
 }
 
 static void
