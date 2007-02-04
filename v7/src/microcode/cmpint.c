0a61d74b6731bdaead24b18a8434fba4366f1437
Taylor R. Campbell <net/mumble/campbell>
2007-02-04 21:55:45 +0000

    Fix bug in compiler reference trap restarts: the name will be at the top of the stack, not in the expression register.

--- v7/src/microcode/cmpint.c
+++ v7/src/microcode/cmpint.c
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: cmpint.c,v 1.107 2007/01/05 21:19:25 cph Exp $
+$Id: cmpint.c,v 1.108 2007/02/04 21:55:45 riastradh Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -1817,7 +1817,7 @@ DEFINE_SCHEME_UTILITY_2 (name, return_address_raw, cache_addr_raw)	\
 C_TO_SCHEME long							\
 DEFUN_VOID (restart)							\
 {									\
-  SCHEME_OBJECT name = exp_register;					\
+  SCHEME_OBJECT name = (STACK_POP ());					\
   SCHEME_OBJECT environment = (STACK_POP ());				\
   long code = (c_lookup (environment, name, (&val_register)));		\
   if (code == PRIM_DONE)						\
