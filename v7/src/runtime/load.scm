4b0c5b33b17f61bb172424d784b45984b5eee076
Chris Hanson <org/chris-hanson/cph>
2007-04-09 16:41:56 +0000

    Reorder LOAD types for consistency with FASLOAD.

--- v7/src/runtime/load.scm
+++ v7/src/runtime/load.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: load.scm,v 14.86 2007/04/07 04:05:07 cph Exp $
+$Id: load.scm,v 14.87 2007/04/09 16:41:56 cph Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -37,10 +37,10 @@ USA.
   (set! load/suppress-loading-message? #f)
   (set! load/default-types
 	`((#f ,wrapper/load/built-in)
-	  ("com" ,load/internal)
 	  ("so" ,load-object-file)
 	  ("sl" ,load-object-file)
 	  ("dylib" ,load-object-file)
+	  ("com" ,load/internal)
 	  ("bin" ,load/internal)
 	  ("scm" ,load/internal)))
   (set! fasload/default-types
