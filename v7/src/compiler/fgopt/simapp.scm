075bb66f4e07f45be8013f0b35b35122cadaf100
Chris Hanson <org/chris-hanson/cph>
2007-04-29 20:17:43 +0000

    Fix typo in previous change.

--- v7/src/compiler/fgopt/simapp.scm
+++ v7/src/compiler/fgopt/simapp.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: simapp.scm,v 4.14 2007/04/29 20:16:05 cph Exp $
+$Id: simapp.scm,v 4.15 2007/04/29 20:17:43 cph Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -118,8 +118,10 @@ USA.
 		     (warn "Procedure called with wrong number of arguments"
 			   value
 			   number-supplied))))
+	      #|
 	      (else
-	       #|(warn "Possible inapplicable operator" operator)|#))))))
+	       (warn "Possible inapplicable operator" operator))
+	      |#)))))
 
 (define (initialize-lvalue-cache! lvalue)
   (set-lvalue-values-cache! lvalue (lvalue-values lvalue)))
