51bcd99969327ec1c8e05c54ae4894d134eacb28
Chris Hanson <org/chris-hanson/cph>
2007-05-13 12:22:47 +0000

    Fix bug: "Makefile.in" is cleaned by maintainer-clean, not by distclean.

--- v7/src/etc/Clean.sh
+++ v7/src/etc/Clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: Clean.sh,v 1.21 2007/05/09 20:01:37 cph Exp $
+# $Id: Clean.sh,v 1.22 2007/05/13 12:22:47 cph Exp $
 #
 # Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
 #     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
@@ -70,9 +70,6 @@ if [ ${DIST} = yes ]; then
     if [ -f Makefile.in ]; then
 	maybe_rm Makefile
     fi
-    if [ -f Makefile-fragment ]; then
-	maybe_rm Makefile.in
-    fi
 fi
 
 if [ ${MAINTAINER} = yes ]; then
@@ -81,6 +78,9 @@ if [ ${MAINTAINER} = yes ]; then
     for FN in Clean.sh Setup.sh Stage.sh Tags.sh; do
 	maybe_unlink "${FN}" "${TOPDIR}/etc/${FN}"
     done
+    if [ -f Makefile-fragment ]; then
+	maybe_rm Makefile.in
+    fi
 fi
 
 for KEYWORD in ${KEYWORDS}; do
