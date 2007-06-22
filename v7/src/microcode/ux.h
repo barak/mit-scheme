e96c53f02d9b614c944f4a411aa8ded20a239fec
Taylor R. Campbell <net/mumble/campbell>
2007-06-22 08:09:25 +0000

    Add initial trivial configuration for OpenBSD.
    This works well enough to build Scheme with liarc; whether there are
    other bugs lurking, I don't know, but I imagine that it is probably OK.

--- v7/src/microcode/ux.h
+++ v7/src/microcode/ux.h
@@ -1,6 +1,6 @@
 /* -*-C-*-
 
-$Id: ux.h,v 1.83 2007/04/22 16:31:23 cph Exp $
+$Id: ux.h,v 1.84 2007/06/22 08:09:25 riastradh Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -74,6 +74,10 @@ USA.
 
 #ifdef _NEXTOS
 #  define SYSTEM_VARIANT "NeXT"
+#endif
+
+#ifdef __OpenBSD__
+#  define SYSTEM_VARIANT "OpenBSD"
 #endif
 
 #ifdef __osf__
