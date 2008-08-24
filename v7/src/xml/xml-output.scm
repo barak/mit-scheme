3fddc015665b007cb9a37105fcbe4418af5df3b8
Chris Hanson <org/chris-hanson/cph>
2008-08-24 06:27:20 +0000

    Rename XML->STRING to XML->OCTETS; define alias for compatibility.

--- v7/src/xml/xml-output.scm
+++ v7/src/xml/xml-output.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: xml-output.scm,v 1.45 2008/07/26 05:45:36 cph Exp $
+$Id: xml-output.scm,v 1.46 2008/08/24 06:27:20 cph Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -39,7 +39,7 @@ USA.
       (set-coding xml port)
       (write-xml-1 xml port options))))
 
-(define (xml->string xml . options)
+(define (xml->octets xml . options)
   (call-with-output-octets
    (lambda (port)
      (set-coding xml port)
