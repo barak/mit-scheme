5067ed63fbc7b945d45dc62ccfa176be30d59ee6
Chris Hanson <org/chris-hanson/cph>
2008-09-21 23:49:05 +0000

    Tweak unparser for requests.

--- v7/src/runtime/httpio.scm
+++ v7/src/runtime/httpio.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: httpio.scm,v 14.11 2008/09/21 22:20:18 cph Exp $
+$Id: httpio.scm,v 14.12 2008/09/21 23:49:05 cph Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -56,7 +56,7 @@ USA.
   (simple-unparser-method 'HTTP-REQUEST
     (lambda (request)
       (list (http-request-method request)
-	    (http-request-uri request)))))
+	    (uri->string (http-request-uri request))))))
 
 (define-record-type <http-response>
     (%make-http-response version status reason headers body)
