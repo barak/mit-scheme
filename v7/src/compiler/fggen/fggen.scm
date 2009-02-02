7f1f0b3b3baf777ee5e8438507467f83291008a7
Taylor R. Campbell <net/mumble/campbell>
2009-02-02 15:33:38 +0000

    When testing for combinations of NOT and GENERAL-CAR-CDR, refer to the primitives, not to the values of the top-level variables by those names.

--- v7/src/compiler/fggen/fggen.scm
+++ v7/src/compiler/fggen/fggen.scm
@@ -1,6 +1,6 @@
 #| -*-Scheme-*-
 
-$Id: fggen.scm,v 4.45 2008/02/14 02:12:14 cph Exp $
+$Id: fggen.scm,v 4.46 2009/02/02 15:33:38 riastradh Exp $
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
@@ -575,11 +575,11 @@ USA.
 (define (generate/combination block continuation context expression)
   (scode/combination-components expression
     (lambda (operator operands)
-      (cond ((eq? not operator)
+      (cond ((eq? (ucode-primitive not) operator)
 	     (generate/conditional block continuation context
 				   (scode/make-conditional (car operands)
 							   #F #T)))
-	    ((and (eq? general-car-cdr operator)
+	    ((and (eq? (ucode-primitive general-car-cdr) operator)
 		  (let ((n (cadr operands)))
 		    (and (exact-integer? n)
 			 (positive? n))))
