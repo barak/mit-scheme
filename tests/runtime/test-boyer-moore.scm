a424d760119cb4924a2afcaa9466a080a735bebf
Chris Hanson <org/chris-hanson/cph>
2009-09-26 21:04:16 -0700

    Add notices and comments to test files.

--- tests/runtime/test-boyer-moore.scm
+++ tests/runtime/test-boyer-moore.scm
@@ -1,25 +1,29 @@
 #| -*-Scheme-*-
 
-$Id$
+Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
+    1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+    2006, 2007, 2008, 2009 Massachusetts Institute of Technology
 
-Copyright (c) 1999 Massachusetts Institute of Technology
+This file is part of MIT/GNU Scheme.
 
-This program is free software; you can redistribute it and/or modify
+MIT/GNU Scheme is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or (at
 your option) any later version.
 
-This program is distributed in the hope that it will be useful, but
+MIT/GNU Scheme is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+along with MIT/GNU Scheme; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
 |#
 
-;;;; Test code for Boyer-Moore String Search
+;;;; Tests for Boyer-Moore String Search
 
 (declare (usual-integrations))
 
--- tests/runtime/test-stream.scm
+++ tests/runtime/test-stream.scm
@@ -1,3 +1,39 @@
+#| -*-Scheme-*-
+
+Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
+    1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+    2006, 2007, 2008, 2009 Massachusetts Institute of Technology
+
+This file is part of MIT/GNU Scheme.
+
+MIT/GNU Scheme is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+MIT/GNU Scheme is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MIT/GNU Scheme; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+USA.
+
+|#
+
+;;;; Test that long streams don't leak memory
+
+(declare (usual-integrations))
+
+(define (run-tests filename)
+  (toggle-gc-notification!)
+  (stream-length (file-stream filename))
+  (stream-length (stream-map (lambda (x) x) (file-stream filename)))
+  (stream-for-each (lambda (x) x) (file-stream filename))
+  (toggle-gc-notification!))
+
 (define (file-stream filename)
   (let ((port (open-input-file filename)))
     (let loop ()
@@ -7,12 +43,3 @@
 	      (close-port port)
 	      '())
 	    (cons-stream char (loop)))))))
-
-(define (run-tests filename)
-  (toggle-gc-notification!)
-  (stream-length (file-stream filename))
-  (stream-length (stream-map (lambda (x) x) (file-stream filename)))
-  (stream-for-each (lambda (x) x) (file-stream filename))
-  (toggle-gc-notification!))
-
-(run-tests "~/gunk/receiver/chip1/receiver/qrm-1.sim")
