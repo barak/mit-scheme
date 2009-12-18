d957da5951bffb7516fdb8aba868c967e07210b7
Chris Hanson <org/chris-hanson/cph>
2009-12-18 00:46:46 -0800

    Tweak command-line options.

--- src/etc/windows/install.bat
+++ src/etc/windows/install.bat
@@ -1,3 +1,3 @@
 copy microcode\scheme.exe c:\local\bin\mit-scheme.exe
-del /s c:\local\mit-scheme
-xcopy /e /y lib\*.* c:\local\mit-scheme
+del /q /s c:\local\mit-scheme
+xcopy /e /i /y lib\*.* c:\local\mit-scheme
