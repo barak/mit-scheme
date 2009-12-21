ea4cdbd9eba2432fcfe0d618e013d770d171b939
Chris Hanson <org/chris-hanson/cph>
2009-12-20 16:16:55 -0800

    Make sure all necessary load-time dirs and debugging info is present.

--- src/etc/windows/make-lib.bat
+++ src/etc/windows/make-lib.bat
@@ -1,3 +1,4 @@
+@echo off
 md lib
 copy etc\optiondb.scm lib
 
@@ -8,3 +9,10 @@ for %%fn in (chrsyn cpress format gdbm hashtb krypt mime-codec numint optiondb o
 md lib\edwin
 copy edwin\*.bci lib\edwin
 for %%fn in (debian-changelog eystep lisppaste manual midas nntp paredit pasmod print pwedit pwparse snr sort techinfo telnet tximod verilog vhdl webster) do copy edwin\%fn%.com lib\edwin
+
+for %%dir in (cref imail sf sos ssp star-parser xml) do (md lib\%dir% & copy %dir%\*.bci %dir%\*.com %dir%\*-w32.pkd lib\%dir%)
+for %%dir in (imail sos ssp star-parser xml) do copy %dir%\load.scm lib\%dir%
+
+md lib\compiler
+md lib\compiler\machines
+for %%dir in (back base fggen fgopt machines\i386 rtlbase rtlgen rtlopt) do (md lib\compiler\%dir% & copy compiler\%dir%\*.bci lib\compiler\%dir%)
