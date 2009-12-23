b008f3ce0114cd603da07afe1d96159c8062d417
Chris Hanson <org/chris-hanson/cph>
2009-12-23 01:44:01 -0800

    Fix problems with file enumerations.

--- src/etc/windows/make-lib.bat
+++ src/etc/windows/make-lib.bat
@@ -4,11 +4,12 @@ copy etc\optiondb.scm lib
 
 md lib\runtime
 copy runtime\*.bci lib\runtime
-for %%fn in (chrsyn cpress format gdbm hashtb krypt mime-codec numint optiondb ordvec pgsql process rbtree regexp rexp rgxcmp syncproc wttree ystep) do copy runtime\%fn%.com lib\runtime
+for %%fn in (chrsyn cpress format mime-codec numint optiondb ordvec process rbtree regexp rexp rgxcmp syncproc wttree ystep) do copy runtime\%fn%.com lib\runtime
 
 md lib\edwin
 copy edwin\*.bci lib\edwin
-for %%fn in (debian-changelog eystep lisppaste manual midas nntp paredit pasmod print pwedit pwparse snr sort techinfo telnet tximod verilog vhdl webster) do copy edwin\%fn%.com lib\edwin
+for %%fn in (artdebug debian-changelog eystep lisppaste manual midas nntp paredit pasmod print pwedit pwparse snr sort techinfo telnet tximod verilog vhdl webster) do copy edwin\%fn%.com lib\edwin
+copy etc\tutorial lib\edwin
 
 for %%dir in (cref imail sf sos ssp star-parser xml) do (md lib\%dir% & copy %dir%\*.bci %dir%\*.com %dir%\*-w32.pkd lib\%dir%)
 for %%dir in (imail sos ssp star-parser xml) do copy %dir%\load.scm lib\%dir%
