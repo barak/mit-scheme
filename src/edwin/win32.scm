67d5b70bd975ca2d8b9ccdd960c9bb109037f784
Joe Marshall <eval.apply@gmail.com>
2010-08-11 09:45:10 -0700

    Add declarations to silence warnings.

--- src/edwin/win32.scm
+++ src/edwin/win32.scm
@@ -174,17 +174,25 @@ USA.
   window type				; ignored
   (set-screen-redisplay-title?! screen #t))
 
+;; (define (win32-screen/scroll-lines-down! screen xl xu yl yu amount)
+;;   (and #f
+;;        (win32-screen-vertical-scroll! (screen->handle screen)
+;; 				      xl xu yl yu (fix:+ yl amount))))
+
 (define (win32-screen/scroll-lines-down! screen xl xu yl yu amount)
-  (and #f
-       (win32-screen-vertical-scroll! (screen->handle screen)
-				      xl xu yl yu (fix:+ yl amount))))
+  (declare (ignore screen xl xu yl yu amount))
+  #f)
+
+;; (define (win32-screen/scroll-lines-up! screen xl xu yl yu amount)
+;;   (and #f
+;;        (win32-screen-vertical-scroll! (screen->handle screen)
+;; 				      xl xu amount yu 0)
+;;        (win32-screen-vertical-scroll! (screen->handle screen)
+;; 				      xl xu yl yu (fix:- yl amount))))
 
 (define (win32-screen/scroll-lines-up! screen xl xu yl yu amount)
-  (and #f
-       (win32-screen-vertical-scroll! (screen->handle screen)
-				      xl xu amount yu 0)
-       (win32-screen-vertical-scroll! (screen->handle screen)
-				      xl xu yl yu (fix:- yl amount))))
+  (declare (ignore screen xl xu yl yu amount))
+  #f)
 
 (define (win32-screen/flush! screen)
   ;; Win32 API call causes any pending painting to be done
