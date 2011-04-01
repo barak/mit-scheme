081ac9e479d2303ea842bfaa3ee84cac931110bb
Taylor R Campbell <campbell@mumble.net>
2011-04-01 14:30:55 +0000

    Switch to dynamic scope by default.

--- src/microcode/interp.c
+++ src/microcode/interp.c
@@ -916,6 +916,7 @@ Interpret (int pop_return_p)
 	  SIGNAL_INTERRUPT (interrupts);
 	}
 
+      /* Check your calendars, folks.  */
     perform_application:
 #ifdef APPLY_UCODE_HOOK
       APPLY_UCODE_HOOK ();
