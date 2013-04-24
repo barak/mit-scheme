5955c53c61b6e6b67792ddaac75cf4e336ec30fc
Matt Birkholz <matt@birkholz.chandler.az.us>
2013-04-23 17:49:31 -0700

    ffi: Temporay hack is no longer needed.

--- src/ffi/compile.scm
+++ src/ffi/compile.scm
@@ -4,22 +4,6 @@ Compile the FFI system. |#
 
 (load-option 'CREF)
 
-;; Temporay hack.  Remove when (runtime ffi) is in the release.
-(if (not (name->package '(RUNTIME FFI)))
-    (let ((path (package-set-pathname "../runtime/runtime")))
-      (if (not (file-exists? path))
-	  (cref/generate-trivial-constructor "../runtime/runtime"))
-      (eval `(for-each-vector-element
-	      (package-file/descriptions (fasload ,path))
-	      (lambda (description)
-		(if (equal? (package-description/name description) '(RUNTIME FFI))
-		    (begin
-		      (construct-normal-package-from-description description)
-		      (create-links-from-description description)
-		      (load "../runtime/ffi" (->environment '(RUNTIME FFI))
-			    'ignored #t)))))
-	    (->environment '(PACKAGE)))))
-
 ;; Temporary hack.  Remove when compile-system is in the release.
 (if (not (environment-bound? (->environment '()) 'compile-system))
     (let ((butil-env (->environment '(cross-reference)))
