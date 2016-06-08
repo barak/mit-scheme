79a80dba057d045713ffb689cc64b7fde8ba2949
Matt Birkholz <puck@birchwood-abbey.net>
2016-06-08 15:01:07 -0700

    x11-screen/x11-screen-check.sh: Test read some X11 properties.

--- src/x11-screen/x11-screen-check.sh
+++ src/x11-screen/x11-screen-check.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+# -*-Scheme-*-
 #
 # Test the X11-SCREEN option.
 
 set -e
 ${MIT_SCHEME_EXE} --prepend-library . <<\EOF
 (begin
-  (load-option 'X11-SCREEN)
-
   (if (let ((display (get-environment-variable "DISPLAY")))
 	(or (not (string? display))
 	    (string-null? display)))
       (warn "DISPLAY not set")
-      (edit))
+      (let ((edwin (->environment '(edwin))))
+	(load-option 'X11-SCREEN)
+	(set! (access os/init-file-name edwin)
+	      (let ((pathname (merge-pathnames "x11-screen-test.scm")))
+		(named-lambda (os/init-file-name/x11-screen-test)
+		  pathname)))
+	(edit)))
   )
 EOF
