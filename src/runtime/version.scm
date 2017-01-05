0294c5a1b27666b0296e6ef4bd33d80e2e6ab2ae
Chris Hanson <org/chris-hanson/cph>
2017-01-04 16:45:39 -0800

    Update copyright licenses for new year.

--- src/runtime/version.scm
+++ src/runtime/version.scm
@@ -2,8 +2,8 @@
 
 Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
     1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-    2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016
-    Massachusetts Institute of Technology
+    2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+    2017 Massachusetts Institute of Technology
 
 This file is part of MIT/GNU Scheme.
 
@@ -31,7 +31,7 @@ USA.
 
 (define copyright-years)
 
-(define last-copyright-year 2016 )
+(define last-copyright-year 2017 )
 
 (add-boot-init!
  (lambda ()
