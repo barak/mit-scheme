1a551b0a9434d9a740d78318ddaff3c9588aa16a
Matt Birkholz <matt@birchwood-abbey.net>
2017-05-17 16:13:47 -0700

    mcrypt-check.sh: Load-option for run-shell-command.

--- src/mcrypt/mcrypt-check.sh
+++ src/mcrypt/mcrypt-check.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# -*-Scheme-*-
 #
 # Test the MCRYPT option.
 
 set -e
 ${MIT_SCHEME_EXE} --prepend-library . <<\EOF
-(load-option 'MCRYPT)
-(load "mcrypt-check" (->environment '(mcrypt)))
+(begin
+  (load-option 'synchronous-subprocess)
+  (load-option 'mcrypt)
+  (load "mcrypt-check" (->environment '(mcrypt))))
 EOF
