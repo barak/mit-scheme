c750643a6d602fbb841169de1e8f55996c4c5d9b
Chris Hanson <org/chris-hanson/cph>
2018-05-13 17:15:59 -0700

    Rename GET-SUPPORTED-FEATURES to FEATURES for R7RS.
    Also add some more R7RS comments to runtime.pkg.

--- src/runtime/mit-macros.scm
+++ src/runtime/mit-macros.scm
@@ -600,7 +600,7 @@ USA.
 (define-feature 'i386 (machine? "IA-32"))
 (define-feature 'x86-64 (machine? "x86-64"))
 
-(define (get-supported-features)
+(define (features)
   (filter-map (lambda (p)
 		(and ((cdr p))
 		     (car p)))
