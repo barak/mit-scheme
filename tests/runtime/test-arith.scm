f15d1d945fcd7406888ea62791b8adec1f637ae4
Taylor R Campbell <campbell@mumble.net>
2018-12-14 16:12:09 +0000

    Relax expm1 tests; i387 in 53-bit precision won't guarantee 0.5ulp.

--- tests/runtime/test-arith.scm
+++ tests/runtime/test-arith.scm
@@ -282,9 +282,7 @@ USA.
   (list
    (list 0 0)
    (list (log 2) 1.)
-   (list 1 (- (exp 1) 1))
-   (list (expt 2 -53) (expt 2. -53))
-   (list (- (expt 2 -53)) (- (expt 2. -53))))
+   (list 1 (- (exp 1) 1)))
   (lambda (x y)
     (assert-eqv (expm1 x) y)))
 
@@ -300,7 +298,9 @@ USA.
    (list -0.6 -0.45118836390597356)
    (list 0.6 .8221188003905089)
    (list (log 2) 1.)
-   (list 0.7 1.0137527074704766))
+   (list 0.7 1.0137527074704766)
+   (list (expt 2 -53) (expt 2. -53))
+   (list (- (expt 2 -53)) (- (expt 2. -53))))
   (lambda (x y)
     (assert-<= (relerr y (expm1 x)) 1e-15)))
 
