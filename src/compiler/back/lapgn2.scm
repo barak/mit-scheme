7ec69d970111ba66af76adb3d951344639824667
Taylor R Campbell <campbell+mit-scheme@mumble.net>
2020-11-25 16:32:23 +0000

    Revert "compiler/back: Don't bother saving aliases for dead registers."
    This reverts commit d248519275f7b9e406a5f09e4f882cc917192946.
    
    This doesn't work after all, because the register might be needed by
    _this_ instruction and might not have been copied to another alias or
    saved to its home yet.
    
    The issue I was trying to address with this change is that in
    
    	(load-machine-register! source rax),
    
    if source's only alias is rax and source is dead afterward, in our
    zeal to clear rax we always allocate another alias for source (or
    save it to its home) even if we're not actually going to use it again
    later on.  We'll have to do that another way.

--- src/compiler/back/lapgn2.scm
+++ src/compiler/back/lapgn2.scm
@@ -314,8 +314,7 @@ USA.
 	  ((map *register-map*)
 	   (needed-registers (eqv-set-union registers *needed-registers*))
 	   (registers registers))
-	(save-machine-register map needed-registers *dead-registers*
-			       (car registers)
+	(save-machine-register map needed-registers (car registers)
 	  (lambda (map needed-registers instructions)
 	    (let ((map (delete-machine-register map (car registers))))
 	      (if (null? (cdr registers))
