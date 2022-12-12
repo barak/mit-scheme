26e0c62afb3802f1f529505d97dfe15b3f12e1ec
Chris Hanson <org/chris-hanson/cph>
2022-12-12 02:23:40 -0800

    Update version now that release-12 branch is cut.

--- src/runtime/version.scm
+++ src/runtime/version.scm
@@ -43,7 +43,7 @@ USA.
 
 (add-boot-init!
  (lambda ()
-   (add-subsystem-identification! "Release" '(12 0 50))
+   (add-subsystem-identification! "Release" '(13 0 50))
    (run-now-and-after-restore!
     (lambda ()
       (add-subsystem-identification! "Microcode" (get-microcode-version))))
