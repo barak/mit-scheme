26b987015af44e968e877d6480648e19d49e60ba
Chris Hanson <org/chris-hanson/cph>
2023-08-25 20:58:31 -0400

    Ignore "v8" dir in project operations.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
-((scheme-mode (whitespace-line-column . 80)
+((nil (project-vc-ignores "v8/"))
+ (scheme-mode (whitespace-line-column . 80)
               (fill-column . 80)
 	      (indent-tabs-mode . t)))
