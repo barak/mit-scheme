5469da54e1a5a22ac2273ba16029a91b774f836a
Chris Hanson <org/chris-hanson/cph>
2023-08-28 15:52:00 -0400

    Add .ccls-cache/ to list of project ignores.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-((nil (project-vc-ignores "v8/"))
+((nil (project-vc-ignores "v8/" ".ccls-cache/"))
  (scheme-mode (whitespace-line-column . 80)
               (fill-column . 80)
 	      (indent-tabs-mode . t)))
