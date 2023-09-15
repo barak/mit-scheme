e19b3e816b7490ca9c02a5fb929077bbfaf0ba4b
Chris Hanson <org/chris-hanson/cph>
2023-09-15 16:30:56 -0400

    Tweak line length for printing.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil (project-vc-ignores "v8/" ".ccls-cache/"))
- (scheme-mode (whitespace-line-column . 80)
-              (fill-column . 80)
+ (scheme-mode (whitespace-line-column . 79)
+              (fill-column . 79)
 	      (indent-tabs-mode . t)))
