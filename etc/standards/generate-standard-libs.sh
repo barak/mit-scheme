096b22dc35022298f0bd26e787af70f840906938
Chris Hanson <org/chris-hanson/cph>
2024-03-03 15:42:47 -0500

    Use MIT_SCHEME_EXE when generating standards.

--- etc/standards/generate-standard-libs.sh
+++ etc/standards/generate-standard-libs.sh
@@ -2,6 +2,7 @@
 set -e
 
 HERE=$(dirname "${0}")
-mit-scheme --batch-mode \
-           --load "${HERE}"/standards "${HERE}"/generate-standard-libs \
-           --eval "(generate-standard-libs \"${1}\")" '(exit)'
+${MIT_SCHEME_EXE:-mit-scheme} \
+    --batch-mode \
+    --load "${HERE}"/standards "${HERE}"/generate-standard-libs \
+    --eval "(generate-standard-libs \"${1}\")" '(exit)'
--- etc/standards/generate-standards-doc.sh
+++ etc/standards/generate-standards-doc.sh
@@ -3,6 +3,7 @@
 set -eu
 
 HERE=$(dirname "${0}")
-mit-scheme --batch-mode \
-           --load "${HERE}"/standards "${HERE}"/generate-standards-doc \
-           --eval "(generate-$(basename "$1") \"$1\")" '(exit)'
+${MIT_SCHEME_EXE:-mit-scheme} \
+    --batch-mode \
+    --load "${HERE}"/standards "${HERE}"/generate-standards-doc \
+    --eval "(generate-$(basename "$1") \"$1\")" '(exit)'
