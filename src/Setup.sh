b9cede3c49fffd5d58cb22d25ccd92f13f722a69
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2026-09-15 19:00:07 +0100

    automake lib/ symlink farm generation
    Create the lib/ symlink farm at "make" time
    
    The lib/ symlink farm presents the source tree in the installed
    layout, and is needed by run-build. It was previously created by
    Setup.sh, and thus was in release tarballs. This moves that creation
    to Makefile.am, so it is built at "make" time, with dependencies and
    stamps to ensure it runs at the right time (before the first thing
    that runs the built Scheme), and only once, in both the native and the
    cross paths.
    
    This change moves it out of the release tarball.

--- src/Setup.sh
+++ src/Setup.sh
@@ -65,27 +65,6 @@ autoreconf -i
 INSTALLED_SUBDIRS="cref ffi libraries sf sos ssp star-parser xml"
 OTHER_SUBDIRS="6001 compiler runtime xdoc microcode"
 
-# lib
-maybe_mkdir lib
-maybe_link lib/include ../microcode
-maybe_link lib/mit-scheme.h ../microcode/pruxffi.h
-maybe_link lib/optiondb.scm ../etc/optiondb.scm
-
-maybe_link lib/compiler ../compiler
-maybe_link lib/cref ../cref
-maybe_link lib/edwin ../edwin
-maybe_link lib/ffi ../ffi
-maybe_link lib/imail ../imail
-maybe_link lib/libraries ../libraries
-maybe_link lib/runtime ../runtime
-maybe_link lib/sf ../sf
-maybe_link lib/sos ../sos
-maybe_link lib/ssp ../ssp
-maybe_link lib/star-parser ../star-parser
-maybe_link lib/x11 ../x11
-maybe_link lib/x11-screen ../x11-screen
-maybe_link lib/xml ../xml
-
 maybe_link config.sub microcode/config.sub
 maybe_link config.guess microcode/config.guess
 
